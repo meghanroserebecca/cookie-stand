480754da4b73c4e59ca470ff5b0b596848dd92f9
meghanroserebecca <meghanroserebecca@gmail.com>
2016-11-15 15:29:35 -0800

    finished skeleton for table: all cookiestore instances are adding their data to the dynamic table

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var hours = ['6am', '7am', '8 am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
 function CookieStore(storeName, minHourlyCustomers, maxHourlyCustomers, avgCustomerCookieSale) {
   this.name = storeName;
   this.minHourlyCustomers = minHourlyCustomers;
@@ -33,6 +35,7 @@ CookieStore.prototype.cookiesByTheHour = function () {
 CookieStore.prototype.toHTML = function () {
   var storeTable = document.getElementById('store_table');
   var tableRow = document.createElement('tr');
+  //var blankTableHeader = document
   var nameTableHeader = document.createElement('th');
   var totalTableData = document.createElement('td');
 
@@ -40,11 +43,11 @@ CookieStore.prototype.toHTML = function () {
   tableRow.appendChild(nameTableHeader);
 
   for (var i = 0; i < this.hours.length; i++){
-    var hourlyTableData1 = document.createElement('td');
+    //var hourlyTableData1 = document.createElement('td');
     var hourlyTableData2 = document.createElement('td');
-    hourlyTableData1.textContent = this.hours[i];
+    //hourlyTableData1.textContent = this.hours[i];
     hourlyTableData2.textContent = this.cookiesPerHour[i];
-    tableRow.appendChild(hourlyTableData1);
+    //tableRow.appendChild(hourlyTableData1);
     tableRow.appendChild(hourlyTableData2);
   }
   totalTableData.textContent = this.total;
@@ -53,7 +56,29 @@ CookieStore.prototype.toHTML = function () {
   storeTable.appendChild(tableRow);
 };
 
-var pike = new CookieStore('Pike', 23, 65, 6.3);
+function renderHeaderRow() {
+  var storeTable = document.getElementById('store_table');
+  var tableRow = document.createElement('tr');
+  var blankTableHeader = document.createElement('th');
+  var totalTableHeader = document.createElement('th');
+  var hourlyTableHeader;
+
+  tableRow.appendChild(blankTableHeader);
+
+  for (var i = 0; i < hours.length; i++){
+    hourlyTableHeader = document.createElement('th');
+    hourlyTableHeader.textContent = hours[i];
+    tableRow.appendChild(hourlyTableHeader);
+  }
+  totalTableHeader.textContent = 'Daily Location Total';
+  tableRow.appendChild(totalTableHeader);
+
+  storeTable.appendChild(tableRow);
+}
+
+renderHeaderRow();
+
+var pike = new CookieStore('1st & Pike', 23, 65, 6.3);
 console.log(pike);
 pike.cookiesByTheHour();
 pike.toHTML();
@@ -63,23 +88,27 @@ console.log(pike.total);
 var seaTacAirport = new CookieStore('SeaTac Airport', 3, 23, 1.2);
 console.log(seaTacAirport);
 seaTacAirport.cookiesByTheHour();
+seaTacAirport.toHTML();
 console.log(seaTacAirport.cookiesPerHour);
 console.log(seaTacAirport.total);
 
 var seattleCenter = new CookieStore('Seattle Center', 11, 38, 3.7);
 console.log(seattleCenter);
 seattleCenter.cookiesByTheHour();
+seattleCenter.toHTML();
 console.log(seattleCenter.cookiesPerHour);
 console.log(seattleCenter.total);
 
 var capitolHill = new CookieStore('Capitol Hill', 20, 38, 2.3);
 console.log(capitolHill);
 capitolHill.cookiesByTheHour();
+capitolHill.toHTML();
 console.log(capitolHill.cookiesPerHour);
 console.log(capitolHill.total);
 
 var alki = new CookieStore('Alki', 2, 16, 4.6);
 console.log(alki);
 alki.cookiesByTheHour();
+alki.toHTML();
 console.log(alki.cookiesPerHour);
 console.log(alki.total);
