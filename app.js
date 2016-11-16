dd70dc9a7e0340173dafb9f9c84f0d4dc0bf36e2
meghanroserebecca <meghanroserebecca@gmail.com>
2016-11-15 16:53:56 -0800

    Everything appends to table in index.htm; all elements in place other than footer & styling; some proto-code for footer commented out.

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ CookieStore.prototype.cookiesByTheHour = function () {
 };
 
 CookieStore.prototype.toHTML = function () {
+  this.cookiesByTheHour();
   var storeTable = document.getElementById('store_table');
   var tableRow = document.createElement('tr');
   //var blankTableHeader = document
@@ -80,35 +81,56 @@ renderHeaderRow();
 
 var pike = new CookieStore('1st & Pike', 23, 65, 6.3);
 console.log(pike);
-pike.cookiesByTheHour();
 pike.toHTML();
 console.log(pike.cookiesPerHour);
 console.log(pike.total);
 
 var seaTacAirport = new CookieStore('SeaTac Airport', 3, 23, 1.2);
 console.log(seaTacAirport);
-seaTacAirport.cookiesByTheHour();
 seaTacAirport.toHTML();
 console.log(seaTacAirport.cookiesPerHour);
 console.log(seaTacAirport.total);
 
 var seattleCenter = new CookieStore('Seattle Center', 11, 38, 3.7);
 console.log(seattleCenter);
-seattleCenter.cookiesByTheHour();
 seattleCenter.toHTML();
 console.log(seattleCenter.cookiesPerHour);
 console.log(seattleCenter.total);
 
 var capitolHill = new CookieStore('Capitol Hill', 20, 38, 2.3);
 console.log(capitolHill);
-capitolHill.cookiesByTheHour();
 capitolHill.toHTML();
 console.log(capitolHill.cookiesPerHour);
 console.log(capitolHill.total);
 
 var alki = new CookieStore('Alki', 2, 16, 4.6);
 console.log(alki);
-alki.cookiesByTheHour();
 alki.toHTML();
 console.log(alki.cookiesPerHour);
 console.log(alki.total);
+
+/*var cookieStoreHourlySales = [pike, seaTacAirport, seattleCenter, capitolHill, alki];
+
+function sumHourlyTotals(){
+  result = [];
+  for (var i = 0; i < cookieStoreHourlySales.length; i++) {
+    return cookieStoreHourlySales[i].cookiesPerHour;
+    for (var k = 0; k < cookiesPerHour.length; k++)
+
+  };
+}
+
+sumHourlyTotals();
+/*function renderFooterRow() {
+  var storeTable = document.getElementById('store_table');
+  var tableRow = document.createElement('tr');
+  var totalTableFooter = document.createElement('th');
+  var hourTotalsTableFooter;
+
+  tableRow.appendChild(totalTableFooter);
+
+  for (var i = 0; i < hours.length; i++) {
+    hourTotalsTableFooter = document.createElement('td');
+    hourTotalsTableFooter.textContent =
+  }
+}*/
