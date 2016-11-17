ea457879dfeedef3a1c4cf078a1f7197acbd4099
meghanroserebecca <meghanroserebecca@gmail.com>
2016-11-16 18:05:55 -0800

    DRY'd up some code, plugged away (fruitlessly) at adding the footer hour totals, figured out and added code to push the new stores generated by the form into the storeList array

--- app.js
+++ app.js
@@ -36,7 +36,6 @@ CookieStore.prototype.toHTML = function () {
   this.cookiesByTheHour();
   var storeTable = document.getElementById('store_table');
   var tableRow = document.createElement('tr');
-  //var blankTableHeader = document
   var nameTableHeader = document.createElement('th');
   var totalTableData = document.createElement('td');
 
@@ -80,34 +79,20 @@ function renderHeaderRow() {
 renderHeaderRow();
 
 var pike = new CookieStore('1st & Pike', 23, 65, 6.3);
-console.log(pike);
-pike.toHTML();
-console.log(pike.cookiesPerHour);
-console.log(pike.total);
 
 var seaTacAirport = new CookieStore('SeaTac Airport', 3, 23, 1.2);
-console.log(seaTacAirport);
-seaTacAirport.toHTML();
-console.log(seaTacAirport.cookiesPerHour);
-console.log(seaTacAirport.total);
 
 var seattleCenter = new CookieStore('Seattle Center', 11, 38, 3.7);
-console.log(seattleCenter);
-seattleCenter.toHTML();
-console.log(seattleCenter.cookiesPerHour);
-console.log(seattleCenter.total);
 
 var capitolHill = new CookieStore('Capitol Hill', 20, 38, 2.3);
-console.log(capitolHill);
-capitolHill.toHTML();
-console.log(capitolHill.cookiesPerHour);
-console.log(capitolHill.total);
 
 var alki = new CookieStore('Alki', 2, 16, 4.6);
-console.log(alki);
-alki.toHTML();
-console.log(alki.cookiesPerHour);
-console.log(alki.total);
+
+var storeList = [pike, seaTacAirport, seattleCenter, capitolHill, alki];
+
+for (var i = 0; i < storeList.length; i++) {
+  storeList[i].toHTML();
+};
 
 var storeForm = document.getElementById('chat_form');
 var messages = [];
@@ -130,35 +115,22 @@ function handleStoreSubmit(event) {
     console.log(newStore);
     newStore = new CookieStore(storeName, minHourlyCustomers, maxHourlyCustomers, avgCustomerCookieSale);
     newStore.toHTML();
-
+    storeList.push(newStore);
+    console.log(storeList);
     event.target.store_name.value = '';
     event.target.min_cust.value = '';
     event.target.max_cust.value = '';
     event.target.avg_cookies.value = '';}
 }
 
-/*var cookieStoreHourlySales = [pike, seaTacAirport, seattleCenter, capitolHill, alki];
-
-function sumHourlyTotals(){
-  result = [];
-  for (var i = 0; i < cookieStoreHourlySales.length; i++) {
-    return cookieStoreHourlySales[i].cookiesPerHour;
-    for (var k = 0; k < cookiesPerHour.length; k++)
-
-  };
+/*function sumHourlyTotals() {
+  results = [];
+  for (var i = 0; i < storeList.length; i++) {
+    return results.push(storeList[i].cookiesPerHour);
+  }
+  for (var k = 0; k < cookiesPerHour.length; k++) {
+    return cookiesPerHour[i];
+  }
 }
 
-sumHourlyTotals();
-/*function renderFooterRow() {
-  var storeTable = document.getElementById('store_table');
-  var tableRow = document.createElement('tr');
-  var totalTableFooter = document.createElement('th');
-  var hourTotalsTableFooter;
-
-  tableRow.appendChild(totalTableFooter);
-
-  for (var i = 0; i < hours.length; i++) {
-    hourTotalsTableFooter = document.createElement('td');
-    hourTotalsTableFooter.textContent =
-  }
-}*/
+sumHourlyTotals();*/
